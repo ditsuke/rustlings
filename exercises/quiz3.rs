c387e64e72fd07ea0c1f0b3e074d6d193793f899
ditsuke <ditsuke@protonmail.com>
2022-08-15 21:55:44 +0530

    quiz3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -14,18 +14,28 @@
 
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use std::fmt::Display;
 
-pub struct ReportCard {
-    pub grade: f32,
+trait GradeType: std::fmt::Display {}
+
+impl GradeType for f32 {}
+impl GradeType for &str {}
+
+struct ReportCard<T>
+where
+    T: GradeType,
+{
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: GradeType> ReportCard<T> {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade
+        )
     }
 }
 
@@ -50,7 +60,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
