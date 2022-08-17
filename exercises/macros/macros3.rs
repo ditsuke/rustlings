c9baaef8379e538b6a8fb558373841b1114b8ea7
ditsuke <ditsuke@protonmail.com>
2022-08-17 23:52:44 +0530

    macros

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -2,14 +2,19 @@
 // Make me compile, without taking the macro out of the module!
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
+// either this **
+#[macro_use]
 mod macros {
+    // or this **
+    #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
     }
+
+    // or even this (latest thing?) **
+    pub(crate) use my_macro;
 }
 
 fn main() {
